2b27f8238464ed6d525206b28e37bfb09da47098
Baiur Yuliia <juliy14497@gmail.com>
2024-09-09 19:41:24 +0300

    Add logging to collect productivity information

--- pomodoro.py
+++ pomodoro.py
@@ -1,15 +1,18 @@
 import winsound
 import datetime
 import time
+import logging
+
+logging.basicConfig(level=logging.INFO, filename="history.txt", filemode="a", format="%(asctime)s %(levelname)s %(message)s")
 
 work_time = int(input('Рабочее время, мин: '))
 break_time = int(input('Время перерыва, мин: '))
-repetition = int(input('Количество повторений:'))
+repetition = int(input('Количество повторений: '))
 
 # start
 winsound.Beep(300, 1000)
 
-print(f'Start pomodoro {datetime.datetime.now()}. {repetition} repetition, work time: {work_time}, break time: {break_time}')
+logging.info(f'Start. {repetition} repetition, work time: {work_time}, break time: {break_time}')
 
 while repetition:
     repetition -= 1
@@ -17,9 +20,11 @@ while repetition:
     # work time
     time.sleep(work_time * 60)
     winsound.Beep(300, 1000)
+    logging.info('Work time end')
 
     # break time
     time.sleep(break_time * 60)
     winsound.Beep(300, 1000)
+    logging.info('Break time end')
 
-print(f'End pomodoro {datetime.datetime.now()}. {repetition} repetition, work time: {work_time}, break time: {break_time}')
+logging.info(f'End. {repetition} repetition, work time: {work_time}, break time: {break_time}')
